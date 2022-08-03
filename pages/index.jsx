fbaf6f52a814cbd84737b25e3cd03a45c4508c52
sarangan_E <sarangan@smitiv.co>
2022-08-03 10:41:29 +0530

    Small Changes

--- pages/index.jsx
+++ pages/index.jsx
@@ -38,7 +38,7 @@ const Home = () => {
           referrerpolicy="no-referrer"
         />
         <link
-          href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800"
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800&swap"
           rel="stylesheet"
         ></link>
       </Head>
